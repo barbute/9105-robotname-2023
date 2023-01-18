cfcf0fb9f7457d3db985da4a204e2cc16f6d1648
Endmxn <endman3040@gmail.com>
2023-01-17 18:52:19 -0600

    encoders, gyro, and motor controllers should work

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,18 +1,16 @@
-/*
- * Notes: Update encoders, gyro, and
- * motor controlers once we get 'em.
- */
+//Should work maybe :)
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-//import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
@@ -23,159 +21,145 @@ import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
 
-  private WPI_VictorSPX leftFront;
-  private WPI_VictorSPX leftBack;
-  private WPI_VictorSPX rightFront;
-  private WPI_VictorSPX rightBack;
-/* 
-  private Encoder leftEncoder;
-  private Encoder rightEncoder;
-*/
+  private CANSparkMax leftFront;
+  private CANSparkMax leftBack;
+  private CANSparkMax rightFront;
+  private CANSparkMax rightBack;
+
+  RelativeEncoder leftEncoder;
+  RelativeEncoder rightEncoder;
+
   private DifferentialDrive robotDrive;
-  //^
   private final DifferentialDriveOdometry robotOdometry;
 
   MotorControllerGroup leftMotors = new MotorControllerGroup(leftBack, leftFront);
   MotorControllerGroup rightMotors = new MotorControllerGroup(rightBack, rightFront);
 
-  //^
+  
   private static final Gyro navX = new AHRS(SPI.Port.kMXP);
 
   public DriveSubsystem() {
 
-    leftFront  = new WPI_VictorSPX(Constants.leftFrontMotor);
-    leftBack = new WPI_VictorSPX(Constants.leftBackMotor);
-    rightFront = new WPI_VictorSPX(Constants.rightFrontMotor);
-    rightBack = new WPI_VictorSPX(Constants.rightBackMotor);
+    leftFront = new CANSparkMax(Constants.leftFrontMotor, 
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftBack = new CANSparkMax(Constants.leftBackMotor, 
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightFront = new CANSparkMax(Constants.rightFrontMotor, 
+      CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightBack = new CANSparkMax(Constants.rightBackMotor, 
+      CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    leftFront.setNeutralMode(NeutralMode.Brake);
-    leftBack.setNeutralMode(NeutralMode.Brake);
-    rightFront.setNeutralMode(NeutralMode.Brake);
-    rightBack.setNeutralMode(NeutralMode.Brake);
+    leftFront.setIdleMode(IdleMode.kBrake);
 
-    //Add proper encoders here
+    leftEncoder = leftFront.getEncoder();
+    rightEncoder = rightFront.getEncoder();
 
-    //Set encoder position conversion factors here
-    //Set encoder velocity conversion factors here
+    leftEncoder.setPositionConversionFactor(Constants.kLinearDistanceConversionFactor);
+    rightEncoder.setPositionConversionFactor(Constants.kLinearDistanceConversionFactor);
+
+    leftEncoder.setVelocityConversionFactor(Constants.kLinearDistanceConversionFactor);
+    rightEncoder.setVelocityConversionFactor(Constants.kLinearDistanceConversionFactor);
+
+    leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
 
     leftBack.follow(leftFront);
     rightBack.follow(rightFront);
 
     leftMotors.setInverted(true);
 
-    //^
     navX.reset();
-    //^
     navX.calibrate();
-    //^
     resetEncoders();
 
     robotDrive = new DifferentialDrive(leftMotors, rightMotors);
     //Replace 0's with encoders
     //^
-    robotOdometry = new DifferentialDriveOdometry(navX.getRotation2d(), 0, 0);
-    //^
+    robotOdometry = new DifferentialDriveOdometry(navX.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
     robotOdometry.resetPosition(navX.getRotation2d(), getLeftEncoderPosition(), getHeading(), new Pose2d());
   }
 
   public void resetEncoders() {
-    //rightEncoder.setPosition(0);
-    //leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
+    leftEncoder.setPosition(0);
   }
 
   public void arcadeDrive(double speed, double rotation) {
     robotDrive.arcadeDrive(speed, rotation);
   }
 
-  //^
   public double getRightEncoderPosition() {
-    //return rightEncoder.getPosition();
-    return 1.1;
+    return rightEncoder.getPosition();
   }
   
-  //^
   public double getLeftEncoderPosition() {
-    //return leftEncoder.getPosition();
-    return 1.1;
+    return leftEncoder.getPosition();
   }
 
-  //^
   public double getRightEncoderVelocity() {
-    //return rightEncoder.getVelocity();
-    return 1.1;
+    return rightEncoder.getVelocity();
   }
 
-  //^
   public double getLeftEncoderVelocity() {
-    //return -leftEncoder.getVelocity();
-    return 1.1;
+    return -leftEncoder.getVelocity();
   }
 
-  //^
   public double getTurnRate() {
-    //return -navX.getRate();
-    return 1.1;
+    return -navX.getRate();
   }
 
-  //^
   public static double getHeading() {
-    //return navX.getRotation2d().getDegrees();
-    return 1.1;
+    return navX.getRotation2d().getDegrees();
   }
 
-  //^
   public Pose2d getPose() {
     return robotOdometry.getPoseMeters();
   }
 
-  //^
   public void resetOdometry(Pose2d pose) {
     resetEncoders();
     robotOdometry.resetPosition(navX.getRotation2d(), getLeftEncoderPosition(), getHeading(), pose);
   }
 
-  //^
   public DifferentialDriveWheelSpeeds getWheelSpeeds() {
     return new DifferentialDriveWheelSpeeds(getLeftEncoderVelocity(), getRightEncoderVelocity());
   }
 
-  //^
   public void tankDriveVolts(double leftVolts, double rightVolts) {
     leftMotors.setVoltage(leftVolts);
     rightMotors.setVoltage(rightVolts);
     robotDrive.feed();
   }
 
-  //^
   public double averageEncoderDistance() {
     return ((getRightEncoderPosition() + getLeftEncoderPosition()) / 2.0);
   }
 
-  //Create get encoder methods here
+  public RelativeEncoder getRightEncoder() {
+    return rightEncoder;
+  }
 
-  //^
+  public RelativeEncoder getLeftEncoder() {
+    return leftEncoder;
+  }
+
   public void setMaxOutput(double MaxOutput) {
     robotDrive.setMaxOutput(MaxOutput);
   }
 
-  //^
   public static void zeroHeading() {
     navX.reset();
     navX.calibrate();
   }
 
-  //^
   public Gyro getGyro() {
     return getGyro();
   }
 
   @Override
   public void periodic() {
-    //Replace 0's with encoders
-    //^
-    robotOdometry.update(navX.getRotation2d(), 0, 0);
+    robotOdometry.update(navX.getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
 
-    //^
     SmartDashboard.putNumber("Left encoder value meters", getLeftEncoderPosition());
     SmartDashboard.putNumber("Right encoder value meters", getRightEncoderPosition());
     SmartDashboard.putNumber("Gyro heading", getHeading());
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,22 +1,23 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase{
 
-    private WPI_VictorSPX arm;
-    private WPI_VictorSPX grabber;
+    private CANSparkMax arm;
+    private CANSparkMax grabber;
 
     public IntakeSubsystem() {
-        arm = new WPI_VictorSPX(Constants.armMotor);
-        grabber = new WPI_VictorSPX(Constants.grabMotor);
+        arm = new CANSparkMax(Constants.armMotor, CANSparkMaxLowLevel.MotorType.kBrushless);
+        grabber = new CANSparkMax(Constants.grabMotor, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-        arm.setNeutralMode(NeutralMode.Brake);
-        grabber.setNeutralMode(NeutralMode.Brake);
+        arm.setIdleMode(IdleMode.kBrake);
+        grabber.setIdleMode(IdleMode.kBrake);
     }
 
     public void lift() {
